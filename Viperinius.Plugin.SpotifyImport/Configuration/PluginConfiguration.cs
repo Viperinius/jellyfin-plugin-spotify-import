9d9cb6b2d333acdda8798b943f78c33e0838199c
Viperinius <viperinius@gmx.de>
2023-02-19 23:26:08 +0100

    add playlist ids to configuration and to html page (without saving yet)

--- Viperinius.Plugin.SpotifyImport/Configuration/PluginConfiguration.cs
+++ Viperinius.Plugin.SpotifyImport/Configuration/PluginConfiguration.cs
@@ -1,3 +1,7 @@
+#pragma warning disable CA1819
+
+using System;
+using System.Linq;
 using MediaBrowser.Model.Plugins;
 
 namespace Viperinius.Plugin.SpotifyImport.Configuration;
@@ -14,6 +18,7 @@ public class PluginConfiguration : BasePluginConfiguration
     {
         SpotifyClientId = string.Empty;
         SpotifyClientSecret = string.Empty;
+        PlaylistIds = Array.Empty<string>();
     }
 
     /// <summary>
@@ -25,4 +30,34 @@ public class PluginConfiguration : BasePluginConfiguration
     /// Gets or sets the Spotify client secret.
     /// </summary>
     public string SpotifyClientSecret { get; set; }
+
+    /// <summary>
+    /// Gets or sets the targeted playlist IDs.
+    /// </summary>
+    public string[] PlaylistIds { get; set; }
+
+    /// <summary>
+    /// Adds a new playlist ID.
+    /// </summary>
+    /// <param name="id">The targeted ID.</param>
+    public void AddPlaylistId(string id)
+    {
+        if (!PlaylistIds.Contains(id))
+        {
+            var list = PlaylistIds.ToList();
+            list.Add(id);
+            PlaylistIds = list.ToArray();
+        }
+    }
+
+    /// <summary>
+    /// Removes a playlist ID.
+    /// </summary>
+    /// <param name="id">The targeted ID.</param>
+    public void RemovePlaylistId(string id)
+    {
+        var list = PlaylistIds.ToList();
+        list.Remove(id);
+        PlaylistIds = list.ToArray();
+    }
 }
