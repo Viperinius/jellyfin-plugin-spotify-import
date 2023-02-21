2932cbd6cba2859116dee0f871893b9556d5d720
Viperinius <viperinius@gmx.de>
2023-02-21 19:21:14 +0100

    implement scheduled task for spotify

--- Viperinius.Plugin.SpotifyImport/Tasks/SpotifyImportTask.cs
+++ Viperinius.Plugin.SpotifyImport/Tasks/SpotifyImportTask.cs
@@ -4,7 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Playlists;
 using MediaBrowser.Model.Tasks;
+using Microsoft.Extensions.Logging;
+using Viperinius.Plugin.SpotifyImport.Spotify;
 
 namespace Viperinius.Plugin.SpotifyImport.Tasks
 {
@@ -13,6 +17,30 @@ namespace Viperinius.Plugin.SpotifyImport.Tasks
     /// </summary>
     public class SpotifyImportTask : IScheduledTask
     {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly IPlaylistManager _playlistManager;
+        private readonly ILibraryManager _libraryManager;
+        private readonly IUserManager _userManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpotifyImportTask"/> class.
+        /// </summary>
+        /// <param name="loggerFactory">Instance of the <see cref="ILoggerFactory"/> interface.</param>
+        /// <param name="playlistManager">Instance of the <see cref="IPlaylistManager"/> interface.</param>
+        /// <param name="libraryManager">Instance of the <see cref="ILibraryManager"/> interface.</param>
+        /// <param name="userManager">Instance of the <see cref="IUserManager"/> interface.</param>
+        public SpotifyImportTask(
+            ILoggerFactory loggerFactory,
+            IPlaylistManager playlistManager,
+            ILibraryManager libraryManager,
+            IUserManager userManager)
+        {
+            _loggerFactory = loggerFactory;
+            _playlistManager = playlistManager;
+            _libraryManager = libraryManager;
+            _userManager = userManager;
+        }
+
         /// <inheritdoc/>
         public string Name => "Import Spotify playlists";
 
@@ -26,9 +54,26 @@ namespace Viperinius.Plugin.SpotifyImport.Tasks
         public string Category => "Playlists";
 
         /// <inheritdoc/>
-        public Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
+        public async Task ExecuteAsync(IProgress<double> progress, CancellationToken cancellationToken)
         {
-            return Task.CompletedTask;
+            var playlistIds = Plugin.Instance?.Configuration.PlaylistIds.ToList() ?? new List<string>();
+
+            if (!playlistIds.Any())
+            {
+                return;
+            }
+
+            var spotify = new SpotifyPlaylistProvider(_loggerFactory.CreateLogger<SpotifyPlaylistProvider>(), _loggerFactory.CreateLogger<SpotifyLogger>());
+            spotify.SetUpProvider();
+            await spotify.PopulatePlaylists(playlistIds, cancellationToken).ConfigureAwait(false);
+
+            var playlistSync = new PlaylistSync(
+                                                _loggerFactory.CreateLogger<PlaylistSync>(),
+                                                _playlistManager,
+                                                _libraryManager,
+                                                _userManager,
+                                                spotify.Playlists);
+            await playlistSync.Execute(cancellationToken).ConfigureAwait(false);
         }
 
         /// <inheritdoc/>
