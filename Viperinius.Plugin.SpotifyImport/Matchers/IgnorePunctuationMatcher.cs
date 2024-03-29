9c1ec9f390e061660beccb15030c270052efcacd
Viperinius <viperinius@gmx.de>
2024-03-29 19:06:24 +0100

    fix matching of tracks that use hyphens in spotify but parens in other providers

--- Viperinius.Plugin.SpotifyImport/Matchers/IgnorePunctuationMatcher.cs
+++ Viperinius.Plugin.SpotifyImport/Matchers/IgnorePunctuationMatcher.cs
@@ -10,6 +10,7 @@ namespace Viperinius.Plugin.SpotifyImport.Matchers
     internal class IgnorePunctuationMatcher : IItemMatcher<string>
     {
         private static readonly Regex _regex = new Regex(@"\p{P}");
+        private static readonly Regex _whitespaceRegex = new Regex(@"\s+");
 
         public bool IsStrict => false;
 
@@ -17,6 +18,11 @@ namespace Viperinius.Plugin.SpotifyImport.Matchers
         {
             var i = _regex.Replace(item, string.Empty);
             var t = _regex.Replace(target, string.Empty);
+
+            // replace multiple whitespace chars with one
+            i = _whitespaceRegex.Replace(i, " ");
+            t = _whitespaceRegex.Replace(t, " ");
+
             return new CaseInsensitiveMatcher().Matches(t, i);
         }
     }
