ab822e0800b32083038f29f160f46591189697ff
Viperinius <viperinius@gmx.de>
2025-01-27 21:14:30 +0100

    fix some codeql warnings

--- Viperinius.Plugin.SpotifyImport.Tests/Db/InitTests.cs
+++ Viperinius.Plugin.SpotifyImport.Tests/Db/InitTests.cs
@@ -11,7 +11,7 @@ namespace Viperinius.Plugin.SpotifyImport.Tests.Db
 {
     public class InitTests
     {
-        private static Dictionary<string, List<(string, string)>> _tableCols = new Dictionary<string, List<(string, string)>>
+        private static readonly Dictionary<string, List<(string, string)>> _tableCols = new Dictionary<string, List<(string, string)>>
         {
             { "ProviderPlaylists", [("Id", "INTEGER"), ("ProviderId", "TEXT"), ("PlaylistId", "TEXT"), ("LastState", "TEXT"), ("LastTimestamp", "TEXT")] },
             { "ProviderTracks", [("Id", "INTEGER"), ("ProviderId", "TEXT"), ("TrackId", "TEXT"), ("Name", "TEXT"), ("AlbumName", "TEXT"), ("AlbumArtistNames", "TEXT"), ("ArtistNames", "TEXT"), ("Number", "INTEGER"), ("IsrcId", "TEXT")] },
@@ -61,7 +61,7 @@ namespace Viperinius.Plugin.SpotifyImport.Tests.Db
                     tableRowCount++;
                     var colRowCount = 0;
                     var name = reader.GetString(0);
-                    Assert.True(_tableCols.ContainsKey(name));
+                    Assert.True(_tableCols.TryGetValue(name, out var colVal));
 
                     using var colCmd = db.WrappedConnection.CreateCommand();
                     colCmd.CommandText = $"SELECT name, type FROM pragma_table_info('{name}')";
@@ -69,7 +69,7 @@ namespace Viperinius.Plugin.SpotifyImport.Tests.Db
                     while (colReader.Read())
                     {
                         colRowCount++;
-                        Assert.Contains((colReader.GetString(0), colReader.GetString(1)), _tableCols[name]);
+                        Assert.Contains((colReader.GetString(0), colReader.GetString(1)), colVal);
                     }
                 }
             }
