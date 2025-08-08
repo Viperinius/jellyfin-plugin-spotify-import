aafdc9758cf7753822960d758db98860a89e0979
Viperinius <viperinius@gmx.de>
2025-08-08 19:52:37 +0200

    show default config page in dashboard menu as well (jf 10.11.0 now uses the first found config page having EnableInMainMenu = true as link to plugin configuration)

--- Viperinius.Plugin.SpotifyImport/Plugin.cs
+++ Viperinius.Plugin.SpotifyImport/Plugin.cs
@@ -81,7 +81,9 @@ namespace Viperinius.Plugin.SpotifyImport
                 new PluginPageInfo
                 {
                     Name = this.Name,
-                    EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
+                    EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace),
+                    EnableInMainMenu = true,
+                    MenuIcon = "music_note",
                 },
                 new PluginPageInfo
                 {
@@ -94,6 +96,7 @@ namespace Viperinius.Plugin.SpotifyImport
                     DisplayName = "Spotify Import - Map",
                     EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.manualMapConfig.html", GetType().Namespace),
                     EnableInMainMenu = true,
+                    MenuIcon = "music_note",
                 },
                 new PluginPageInfo
                 {
